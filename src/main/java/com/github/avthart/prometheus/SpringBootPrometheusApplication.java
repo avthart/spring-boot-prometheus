a3a5416af328ef921bf40e23db40db97aeb7884a
Albert van ’t Hart <albert.vanthart@vanadgroup.com>
2018-11-27 10:29:13 +0100

    added helloworld restcontroller

--- src/main/java/com/github/avthart/prometheus/SpringBootPrometheusApplication.java
+++ src/main/java/com/github/avthart/prometheus/SpringBootPrometheusApplication.java
@@ -2,11 +2,29 @@ package com.github.avthart.prometheus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 
 @SpringBootApplication
+@RestController
 public class SpringBootPrometheusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootPrometheusApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String helloWorld() {
+        String hostName = "";
+        try {
+            hostName = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            // ignore, could not get hostname
+        }
+        return "Hello World [" + hostName + "]";
 	}
 }
